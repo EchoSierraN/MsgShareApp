6bec5d6d8a4726b196b8191d074561644d6347cf
Elias Shemsu <echosierran@gmail.com>
2020-04-09 13:10:59 +0300

    Added Toast message.

--- app/src/main/java/com/example/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/example/msgshareapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.msgshareapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,8 @@ class MainActivity : AppCompatActivity() {
 
         btnShowToast.setOnClickListener {
             Log.i("MainActivity", "Button was clicked.")
+
+            Toast.makeText(this, "Button was clicked!", Toast.LENGTH_SHORT).show()
         }
     }
 
