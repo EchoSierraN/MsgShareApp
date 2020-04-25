dae4a8c3746c055d7aa6062641332cef907f171b
Elias Shemsu <echosierran@gmail.com>
2020-04-25 14:30:44 +0300

    #3.4: Custom RecyclerView Adapter example

---
+++ app/src/main/java/com/example/msgshareapp/Model.kt
@@ -0,0 +1,20 @@
+package com.example.msgshareapp
+
+data class Hobby(var title: String)
+
+object Supplier{
+    var hobbies= listOf<Hobby>(Hobby("Swimming"), Hobby("Reading"),
+        Hobby("Walking"), Hobby("Gaming"),
+        Hobby("Movies"), Hobby("Learning"),
+        Hobby("Sleeping"), Hobby("Browsing internet"),
+        Hobby("Swimming"), Hobby("Reading"),
+        Hobby("Walking"), Hobby("Gaming"),
+        Hobby("Movies"), Hobby("Learning"),
+        Hobby("Sleeping"), Hobby("Browsing internet"),
+        Hobby("Swimming"), Hobby("Reading"),
+        Hobby("Walking"), Hobby("Gaming"),
+        Hobby("Movies"), Hobby("Learning"),
+        Hobby("Sleeping"), Hobby("Browsing internet"))
+
+
+}
